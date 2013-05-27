4508af1570229b43f0d066fff6b378534503af5b
lethosor <stahlalan19@gmail.com>
2013-05-27 15:55:18 -0400

    Fix quotes

--- Diagram.php
+++ Diagram.php
@@ -44,7 +44,7 @@ class DFDTile {
 	function __construct($text) {
 		$lines = preg_split('/\n/', $text);
 		if (count($lines) != 6) {
-			trigger_error('Tag {$lines[0]} does not fit format! Skipping');
+			trigger_error("Tag {$lines[0]} does not fit format! Skipping");
 		}
 		$tag = array();
 		preg_match('/<tile name="(.*?)">/', $lines[0], $tag); 
