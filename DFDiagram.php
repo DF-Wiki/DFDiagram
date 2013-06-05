7f082ac04a7c9eb0dbd79877ea62bceffdfadbad
lethosor <stahlalan19@gmail.com>
2013-06-05 17:33:48 -0400

    Change version number to 0.3

--- DFDiagram.php
+++ DFDiagram.php
@@ -41,7 +41,7 @@ $wgExtensionCredits['DFDiagram'][] = array(
 	'author' =>'Lethosor',
 	'url' => 'https://github.com/lethosor/DFDiagram',
 	'description' => 'Adds support for DF-style diagrams',
-	'version'  => '0.3dev',
+	'version'  => '0.3',
 );
 
 //DEV
