08fa8630b270feaf056acf035284283433add91f
lethosor <stahlalan19@gmail.com>
2013-07-01 14:21:35 -0400

    Clean up Grid.php

--- Grid.php
+++ Grid.php
@@ -1,18 +1,20 @@
-<?php
-/*
- Grid class
-*/
+ <?php
 
 class DGrid {
+	/*
+	 * Grid
+	 *
+	 * Sort of like a 2D array, but simplified for the purposes of this extension
+	 */
 	private $matrix;
 	private $width;
 	private $height;
 	
-	public function __construct(){
+	public function __construct() {
 		$this->matrix = array();
 	}
 	public function get($r, $c){
-		while(count($this->matrix) <= $r){
+		while (count($this->matrix) <= $r) {
 			$this->matrix[] = array();
 		}
 		while (count($this->matrix[$r]) <= $c) {
@@ -20,19 +22,14 @@ class DGrid {
 		}
 		return $this->matrix[$r][$c];
 	}
-	public function set($r, $c, $value){
+	public function set($r, $c, $value) {
 		$this->height = max($this->height, $r+1);
 		$this->width = max($this->width, $c+1);
 		$this->get($r, $c);
 		return $this->matrix[$r][$c] = $value;
 	}
 	public function __get($name) {
-		if($name == 'width') return $this->width;
-		if($name == 'height') return $this->height;
+		if ($name == 'width') return $this->width;
+		if ($name == 'height') return $this->height;
 	}
 }
-/*$g = new DGrid();
-$g->set(2,3,'x');
-$g->set(5,7,'y');
-print "a{$g->get(2,3)}b{$g->get(5,6)}c{$g->get(5,7)}d\n";
-*/
