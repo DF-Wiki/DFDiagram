51798e9e7bbbead11532e62b8dfae8ebe099a21e
lethosor <lethosor@gmail.com>
2014-11-05 16:25:47 -0500

    Clean up tests

--- tests/ParserTest.php
+++ tests/ParserTest.php
@@ -6,179 +6,84 @@ class ParserTest extends PHPUnit_Framework_TestCase {
     function setUp() {
         $this->parser = new DFDParser();
     }
-    function testSingleTokens() {
+    function testTokenize() {
         $this->assertEquals(
-            $this->parser->tokenize('a'),
-            array('a')
+            $this->parser->tokenize('a bc'),
+            array('a', ' ', 'b', 'c')
         );
         $this->assertEquals(
-            $this->parser->tokenize(' '),
-            array(' ')
-        );
-        $this->assertEquals(
-            $this->parser->tokenize('[a]'),
-            array('[a]')
-        );
-        $this->assertEquals(
-            $this->parser->tokenize('[ab]'),
-            array('[ab]')
+            $this->parser->tokenize('[a]b[cd]'),
+            array('[a]', 'b', '[cd]')
         );
     }
-    function testSingleEscapes() {
+    function testTokenizeEscapes() {
         $this->assertEquals(
-            $this->parser->tokenize('\\a'),
-            array('a')
+            $this->parser->tokenize('[ab\\]\\[cd]'),
+            array('[ab][cd]')
         );
         $this->assertEquals(
-            $this->parser->tokenize('\\['),
-            array('[')
+            $this->parser->tokenize('[a\\]]'),
+            array('[a]]')
         );
         $this->assertEquals(
-            $this->parser->tokenize('\\]'),
-            array(']')
-        );
-        $this->assertEquals(
-            $this->parser->tokenize('\\\\'),
-            array('\\')
-        );
-    }
-    /**
-     * @expectedException DFDParserError
-     */
-    function testUnfinishedEscape1() {
-        $this->parser->tokenize('\\');
-    }
-    /**
-     * @expectedException DFDParserError
-     */
-    function testUnfinishedEscape2() {
-        $this->parser->tokenize('\\\\\\');
-    }
-    /**
-     * @expectedException DFDParserError
-     */
-    function testUnfinishedEscape3() {
-        $this->parser->tokenize('a\\');
-    }
-    /**
-     * @expectedException DFDParserError
-     */
-    function testMismatchedOpeningBracket() {
-        $this->parser->tokenize('[');
-    }
-    /**
-     * @expectedException DFDParserError
-     */
-    function testMismatchedClosingBracket() {
-        $this->parser->tokenize(']');
-    }
-
-    function testMultipleTokens() {
-        $this->assertEquals(
-            $this->parser->tokenize('ab'),
-            array('a', 'b')
-        );
-        $this->assertEquals(
-            $this->parser->tokenize('[a]b'),
-            array('[a]', 'b')
-        );
-        $this->assertEquals(
-            $this->parser->tokenize('a[b]'),
-            array('a', '[b]')
-        );
-        $this->assertEquals(
-            $this->parser->tokenize('[a][b]'),
-            array('[a]', '[b]')
+            $this->parser->tokenize('\\a\\[\\]\\\\b'),
+            array('a', '[', ']', '\\', 'b')
         );
         $this->assertEquals(
             $this->parser->tokenize('[a]\\[b\\]'),
             array('[a]', '[', 'b', ']')
         );
         $this->assertEquals(
-            $this->parser->tokenize('[ab][cd]'),
-            array('[ab]', '[cd]')
-        );
-        $this->assertEquals(
-            $this->parser->tokenize('[ab\\]\\[cd]'),
-            array('[ab][cd]')
-        );
-        $this->assertEquals(
-            $this->parser->tokenize('[ab]c[de]'),
-            array('[ab]', 'c', '[de]')
-        );
-    }
-
-    function testSingleNestedTokens() {
-        $this->assertEquals(
-            $this->parser->tokenize('[[a]]'),
-            array('[[a]]')
-        );
-        $this->assertEquals(
-            $this->parser->tokenize('[[a][b]]'),
-            array('[[a][b]]')
-        );
-        $this->assertEquals(
-            $this->parser->tokenize('[[[[[[[[[[[[[[[a]]]]]]]]]]]]]]]'),
-            array('[[[[[[[[[[[[[[[a]]]]]]]]]]]]]]]')
-        );
-        $this->assertEquals(
-            $this->parser->tokenize('[[a][]]'),
-            array('[[a][]]')
-        );
-        $this->assertEquals(
-            $this->parser->tokenize('[[a]\\[\\]]'),
-            array('[[a][]]')
-        );
-        $this->assertEquals(
-            $this->parser->tokenize('[[a\\]\\[]]'),
-            array('[[a][]]')
-        );
-    }
-
-    function testMultipleNestedTokens() {
-        $this->assertEquals(
-            $this->parser->tokenize('[[a]][b]'),
-            array('[[a]]', '[b]')
-        );
-        $this->assertEquals(
-            $this->parser->tokenize('[[a]][[b]]'),
-            array('[[a]]', '[[b]]')
-        );
-        $this->assertEquals(
-            $this->parser->tokenize('[[a]]b[[c]]'),
-            array('[[a]]', 'b', '[[c]]')
-        );
-        $this->assertEquals(
-            $this->parser->tokenize('[[a][b]\\]][c]'),
-            array('[[a][b]]]', '[c]')
-        );
-        $this->assertEquals(
             $this->parser->tokenize('[[a][b]\\\\][c]'),
             array('[[a][b]\\]', '[c]')
-        );
-        $this->assertEquals(
-            $this->parser->tokenize('[abc[d\\]e]\\]]fg'),
-            array('[abc[d]e]]]', 'f', 'g')
-        );
-        $this->assertEquals(
-            $this->parser->tokenize('[abc[d\\]e]\\]]f\\g'),
-            array('[abc[d]e]]]', 'f', 'g')
-        );
-        $this->assertEquals(
-            $this->parser->tokenize('[abc[d\\]e]\\]]f\\\\g'),
-            array('[abc[d]e]]]', 'f', '\\', 'g')
-        );
-        $this->assertEquals(
-            $this->parser->tokenize('[abc[d]ef[g]]'),
-            array('[abc[d]ef[g]]')
-        );
-        $this->assertEquals(
-            $this->parser->tokenize('[abc[d]e]\\[f[g]\\]'),
-            array('[abc[d]e]', '[', 'f', '[g]', ']')
         );
         $this->assertEquals(
             $this->parser->tokenize('[abc[d][]\\]ef\\[]g[[\\\\]\\]]'),
             array('[abc[d][]]ef[]', 'g', '[[\\]]]')
         );
+    }
+    function testTokenizeNesting() {
+        $this->assertEquals(
+            $this->parser->tokenize('[abc[d]ef[g]]'),
+            array('[abc[d]ef[g]]')
+        );
+        $this->assertEquals(
+            $this->parser->tokenize('[[a]][b] [[a][b]] [[a]][[b]]'),
+            array('[[a]]', '[b]', ' ', '[[a][b]]', ' ', '[[a]]', '[[b]]')
+        );
+        $this->assertEquals(
+            $this->parser->tokenize('[[a][]] [[a]\\[\\]] [[a\\]\\[]]'),
+            array('[[a][]]', ' ', '[[a][]]', ' ', '[[a][]]')
+        );
+    }
+    /**
+     * @expectedException DFDParserError
+     */
+    function testTokenizeUnfinishedEscape1() {
+        $this->parser->tokenize('\\');
+    }
+    /**
+     * @expectedException DFDParserError
+     */
+    function testTokenizeUnfinishedEscape2() {
+        $this->parser->tokenize('\\\\\\');
+    }
+    /**
+     * @expectedException DFDParserError
+     */
+    function testTokenizeUnfinishedEscape3() {
+        $this->parser->tokenize('a\\');
+    }
+    /**
+     * @expectedException DFDParserError
+     */
+    function testTokenizeMismatchedOpeningBracket() {
+        $this->parser->tokenize('[');
+    }
+    /**
+     * @expectedException DFDParserError
+     */
+    function testTokenizeMismatchedClosingBracket() {
+        $this->parser->tokenize(']');
     }
 }
